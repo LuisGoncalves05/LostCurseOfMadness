3108ff986a615a9a7aa4c598471386ce2912a122
LuisG05 <lumicosgon.lg@gmail.com>
2025-02-25 22:27:53 +0000

    Lab2 - p1

--- lab2/timer.c
+++ lab2/timer.c
@@ -4,10 +4,53 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t old_config;
+  if (timer_get_conf(timer, &old_config)) return 1;
 
-  return 1;
+  uint8_t sel_timer;
+  switch (timer) {
+    case 0:
+      sel_timer = TIMER_SEL0;
+      break;
+    case 1:
+      sel_timer = TIMER_SEL1;
+      break;
+    case 2:
+      sel_timer = TIMER_SEL2;
+      break;
+    default:
+      return 1;
+  }
+
+  uint8_t new_config = sel_timer | TIMER_LSB_MSB | (old_config & 0xF);
+  if (sys_outb(TIMER_CTRL, new_config)) return 1;
+
+  switch (timer) {
+    case 0:
+      sel_timer = TIMER_0;
+      break;
+    case 1:
+      sel_timer = TIMER_1;
+      break;
+    case 2:
+      sel_timer = TIMER_2;
+      break;
+    default:
+      return 1;
+  }
+
+  if (TIMER_FREQ < freq) return 1;
+  uint16_t frequency = (TIMER_FREQ / freq) & 0xFFFF;
+  
+  uint8_t lsb;
+  if (util_get_LSB(frequency, &lsb)) return 1;
+  if (sys_outb(sel_timer, lsb)) return 1;
+
+  uint8_t msb;
+  if (util_get_MSB(frequency, &msb)) return 1;
+  if (sys_outb(sel_timer, msb)) return 1;
+  
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -30,16 +73,45 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (!st) return 1;
+  uint8_t ctrl = (BIT(1) >> timer) | BIT(5) | BIT(6) | BIT(7);
+  if (sys_outb(TIMER_CTRL, ctrl)) return 1;
+  if (util_sys_inb(TIMER_0 + timer, st)) return 1;
+  return 0;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
+  union timer_status_field_val val;
+  switch(field) {
+    case tsf_all:
+      val.byte = st;
+      break;
+    case tsf_initial:
+      switch ((timer >> 4) & 0x2) {
+        case 0:
+          val.in_mode = INVAL_val; 
+          break;
+        case 1:
+          val.in_mode = LSB_only;
+          break;
+        case 2:
+          val.in_mode = MSB_only;
+          break;
+        case 3:
+          val.in_mode = MSB_after_LSB;
+          break;
+        default:
+          return 1;
+      }
+      break;
+    case tsf_mode:
+      val.count_mode = (timer >> 1) & 0x7;
+      break;
+    case tsf_base: 
+      val.in_mode = timer & 1;
+      break;
+    default:
+      return 1;
+  }
+  return timer_print_config(timer, field, val);
 }
--- lab2/utils.c
+++ lab2/utils.c
@@ -1,24 +1,22 @@
 #include <lcom/lcf.h>
-
 #include <stdint.h>
 
-int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  if (!lsb) return 1;
+  *lsb = val & 0xFF;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (!msb) return 1;
+  *msb = (val & 0xFF00) >> 8;
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  u32_t val;
+  int ret = sys_inb(port, &val);
+  *value = val & 0xFF;
+  return ret;
 }
