45770c29c57916fc3f1ca9425ad9736658937576
Freitas <adavidaf2002@gmail.com>
2025-05-18 02:50:57 +0100

    macros for maze

--- proj/src/model/game/maze.h
+++ proj/src/model/game/maze.h
@@ -2,12 +2,18 @@
 #define MAZE_H
 
 #include <lcom/lcf.h>
+#include "../../controller/video/gpu.h"
+
+#define WALL_COLOR 0x07  // Cor cinza para as paredes
+#define CELL_SIZE 48     // Tamanho de cada célula do labirinto
+
 
 typedef struct Maze Maze;
 
-Maze *create_maze(uint8_t size_x, uint8_t size_y);
+Maze* create_maze(int width, int height);
 void free_maze(Maze* maze);
+int draw_maze(Maze* maze, uint8_t* frame_buffer);
+bool check_collision(Maze* maze, int x, int y, int width, int height);
 
-void print_maze(Maze* maze);
 
 #endif
