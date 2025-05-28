719fbda363036f6fc30592de0600138e23476110
LuisG05 <lumicosgon.lg@gmail.com>
2025-05-28 16:14:04 +0100

    mob follows center of player

--- proj/src/model/level/mob.c
+++ proj/src/model/level/mob.c
@@ -68,26 +68,37 @@ MobState mob_get_state(Mob *mob) {
 
 /* Others section */
 
-void mob_update_state(Mob *mob, uint16_t player_x, uint16_t player_y) {
+void mob_update_state(Mob *mob, uint16_t player_cx, uint16_t player_cy) {
     if (mob == NULL)
         return;
-    uint16_t mob_x = mob->animated_sprite->sprite->x;
-    uint16_t mob_y = mob->animated_sprite->sprite->y;
-    int dx = player_x - mob_x;
-    int dy = player_y - mob_y;
+
+    Sprite *sprite = mob->animated_sprite->sprite;
+    uint16_t mob_x = sprite->x;
+    uint16_t mob_y = sprite->y;
+
+    uint16_t mob_cx = mob_x + sprite->width / 2;
+    uint16_t mob_cy = mob_y + sprite->height / 2;
+
+    int dx = player_cx - mob_cx;
+    int dy = player_cy - mob_cy;
     if (abs(dx) < MOB_RADIUS && abs(dy) < MOB_RADIUS) {
         bool new_direction = dx > 0;
         if (mob->state != MOB_ATTACKING || mob->direction != new_direction) {
-            Sprite *new_sprite = create_sprite(new_direction? (xpm_map_t) mob_attacking_right : (xpm_map_t) mob_attacking_left, mob_x, mob_y, 0, 0);
+            Sprite *new_sprite = create_sprite(new_direction ? (xpm_map_t) mob_attacking_right : (xpm_map_t) mob_attacking_left, mob_x, mob_y, 0, 0);
             mob->animated_sprite = create_animated_sprite(new_sprite, 30, 1);
         }
-        mob->animated_sprite->sprite->xspeed = dx == 0? 0 : dx / abs(dx);
-        mob->animated_sprite->sprite->yspeed = dy == 0? 0 : dy / abs(dy);
+        sprite->xspeed = dx == 0 ? 0 : dx / abs(dx);
+        sprite->yspeed = dy == 0 ? 0 : dy / abs(dy);
         mob->state = MOB_ATTACKING;
         mob->direction = new_direction;
     } else {
-        mob->animated_sprite->sprite->xspeed = 0;
-        mob->animated_sprite->sprite->yspeed = 0;
+        Sprite *new_sprite = create_sprite((xpm_map_t) mob_idle_1, mob_x, mob_y, 0, 0);
+        mob->animated_sprite = create_animated_sprite(new_sprite, 30, 5,
+            (xpm_map_t) mob_idle_2,
+            (xpm_map_t) mob_idle_3,
+            (xpm_map_t) mob_idle_2,
+            (xpm_map_t) mob_idle_1
+        );
         mob->state = MOB_IDLE;
     }
 }
