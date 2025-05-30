d5aaac9f8352bb0c224c8d3f797ff715a0a98b92
Sofia Sousa <up202303662@edu.fe.up.pt>
2025-05-30 12:48:03 +0100

    keys documentation

--- proj/src/model/utils/keys.h
+++ proj/src/model/utils/keys.h
@@ -1,17 +1,17 @@
 #ifndef KEYS_H
 #define KEYS_H
 
-#define KEY_W 0x11
-#define KEY_BREAK_W 0x91
-#define KEY_A 0x1E
-#define KEY_BREAK_A 0x9E
-#define KEY_S 0x1F
-#define KEY_BREAK_S 0x9F
-#define KEY_D 0x20
-#define KEY_BREAK_D 0xA0
-#define KEY_X 0x2D
-#define KEY_C 0x2E
-#define KEY_SPACE 0x39
-#define KEY_ENTER 0x1C
+#define KEY_W 0x11       /**< Makecode for W key */
+#define KEY_BREAK_W 0x91 /**< Break code for W key */
+#define KEY_A 0x1E       /**< Makecode for A key */
+#define KEY_BREAK_A 0x9E /**< Break code for A key */
+#define KEY_S 0x1F       /**< Makecode for S key */
+#define KEY_BREAK_S 0x9F /**< Break code for S key */
+#define KEY_D 0x20       /**< Makecode for D key */
+#define KEY_BREAK_D 0xA0 /**< Break code for D key */
+#define KEY_X 0x2D       /**< Makecode for X key */
+#define KEY_C 0x2E       /**< Makecode for C key */
+#define KEY_SPACE 0x39   /**< Makecode for Space key */
+#define KEY_ENTER 0x1C   /**< Makecode for Enter key */
 
 #endif /* KEYS_H */
