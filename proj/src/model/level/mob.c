a548d42f97c03feeb4a7a515852c3f88096c9785
LuisG05 <lumicosgon.lg@gmail.com>
2025-05-30 01:00:48 +0100

    braces and error checks in mob.c

--- proj/src/model/level/mob.c
+++ proj/src/model/level/mob.c
@@ -12,63 +12,94 @@ struct Mob {
 
 Mob *create_mob(uint16_t x, uint16_t y) {
     Mob *mob = (Mob *) malloc(sizeof(Mob));
-    if (!mob)
+    if (mob == NULL) {
+        printf("create_mob: NULL pointer provided\n");
         return NULL;
-    
+    }
+
     mob->health = MOB_HEALTH;
     int rx = rand() % (CELL_SIZE - 30);
     int ry = rand() % (CELL_SIZE - 23);
     Sprite *new_sprite = create_sprite((xpm_map_t) mob_idle_1, x + rx, y + ry, 0, 0);
+    if (new_sprite == NULL) {
+        printf("create_mob: create_sprite failed\n");
+        free(mob);
+        return NULL;
+    }
     mob->animated_sprite = create_animated_sprite(new_sprite, 30, 5,
                                                   (xpm_map_t) mob_idle_2,
                                                   (xpm_map_t) mob_idle_3,
                                                   (xpm_map_t) mob_idle_2,
                                                   (xpm_map_t) mob_idle_1);
+    if (mob->animated_sprite == NULL) {
+        printf("create_mob: create_animated_sprite failed\n");
+        free(new_sprite);
+        free(mob);
+        return NULL;
+    }
+
     mob->max_speed = MOB_MAX_SPEED;
     mob->state = MOB_IDLE;
     return mob;
 }
 
 void destroy_mob(Mob *mob) {
-    if (!mob)
+    if (mob == NULL) {
+        printf("destroy_mob: NULL pointer provided\n");
         return;
-    destroy_animated_sprite(mob->animated_sprite);
+    }
+    
+    if (destroy_animated_sprite(mob->animated_sprite)) {
+        printf("destroy_mob: destroy_animated_sprite failed\n");
+    }
+    
     free(mob);
 }
 
 /* Getter and setter section */
 
 Sprite *mob_get_sprite(Mob *mob) {
-    if (!mob)
+    if (mob == NULL) {
+        printf("mob_get_sprite: NULL pointer provided\n");
         return NULL;
+    }
 
     return mob->animated_sprite->sprite;
 }
 
 unsigned char mob_get_health(Mob *mob) {
-    if (!mob)
+    if (mob == NULL) {
+        printf("mob_get_health: NULL pointer provided\n");
         return 0;
+    }
+
     return mob->health;
 }
 
 void mob_set_health(Mob *mob, unsigned char health) {
-    if (!mob)
+    if (mob == NULL) {
+        printf("mob_set_health: NULL pointer provided\n");
         return;
-    
+    }
+
     mob->health = health;
 }
 
 int mob_get_max_speed(Mob *mob) {
-    if (!mob)
+    if (mob == NULL) {
+        printf("mob_get_max_speed: NULL pointer provided\n");
         return 0;
+    }
 
     return mob->max_speed;
 }
 
-
 MobState mob_get_state(Mob *mob) {
-    if (!mob)
+    if (mob == NULL) {
+        printf("mob_get_state: NULL pointer provided\n");
         return MOB_DEAD;
+    }
+
     return mob->state;
 }
 
@@ -77,8 +108,10 @@ MobState mob_get_state(Mob *mob) {
 /* Others section */
 
 void mob_update_state(Mob *mob, uint16_t player_cx, uint16_t player_cy) {
-    if (mob == NULL)
+    if (mob == NULL) {
+        printf("mob_update_state: NULL pointer provided\n");
         return;
+    }
 
     if (mob->health == 0) {
         mob->state = MOB_DEAD;
@@ -115,20 +148,6 @@ void mob_update_state(Mob *mob, uint16_t player_cx, uint16_t player_cy) {
         }
         mob->state = MOB_IDLE;
     }
-}
-
-void mob_limit_speed(Mob *mob) {
-    if (mob->animated_sprite->sprite->xspeed >= mob->max_speed) {
-        mob->animated_sprite->sprite->xspeed = mob->max_speed;
-    }
-    if (mob->animated_sprite->sprite->xspeed >= mob->max_speed) {
-        mob->animated_sprite->sprite->xspeed = mob->max_speed;
-    }
-}
-
-void mob_stop(Mob *mob) {
-    mob->animated_sprite->sprite->xspeed = MOB_DEFAULT_SPEED;
-    mob->animated_sprite->sprite->xspeed = MOB_DEFAULT_SPEED;
 }
 
 /* Draw section */
